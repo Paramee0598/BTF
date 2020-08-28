c93587185c1a951068af436a5756ebf4491ac65d
TonG <TonG@TONGTUNPHUK>
2020-08-28 09:15:01 +0700

    Add project files.

---
+++ BTF/Source.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int r, h, d, sp;//r = row, h = height, d = digit, sp = space
+	printf("Enter height: ");
+	scanf_s("%d", &h);
+	///////////up/////////////////
+	for (r = 1; r <= h - 1; r++)
+	{
+		for (d = 1; d <= r; d++)
+			printf("%d", d);
+		for (sp = 1; sp <= 2 * (h - r); sp++)
+			printf(" ");
+		printf("\b");
+		for (d = r; d >= 1; d--)
+			printf("%d", d);
+		printf("\n");
+
+	}
+	////////down///////////////////
+	for (r = h; r >= 1; r--)
+	{
+		for (d = 1; d <= r; d++)
+			printf("%d", d);
+		for (sp = 1; sp <= 2 * (h - r); sp++)
+			printf(" ");
+		printf("\b");
+		for (d = r; d >= 1; d--)
+			printf("%d", d);
+		printf("\n");
+
+	}
+	return 0;
+}
